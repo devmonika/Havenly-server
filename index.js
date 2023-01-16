ee2344a42f5dd226a78175f530e5093fcda2ff49
Monika <77955085+devmonika@users.noreply.github.com>
2023-01-16 10:18:35 +0600

    maximum thing added

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+async function run(){
+    try{
+
+    }
+    finally{
+
+    }
+}
+run().catch(console.log);
+
+
+app.get('/',async(req, res) =>{
+    res.send('server running');
+})
+
+app.listen(port,() =>{
+    console.log(`server running on port ${port}`);
+})
