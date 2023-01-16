120c524735600b51aa808fe03aa6137bde3187ab
Md. Sazzad Hossain <96966545+Sazz07@users.noreply.github.com>
2023-01-17 01:33:13 +0600

    MongoDb setup done

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+// mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gympzpz.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 async function run(){
     try{
